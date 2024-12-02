bad9935111a61eb558e526ca9c85f03eedd74cdf
Alde_24 <132966474+Alde24@users.noreply.github.com>
2024-12-02 01:22:03 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -12,11 +12,14 @@ $(document).ready(function() {
   });
   // Descarga la llave pública
   $('#btn_key').on('click', function() {
-    const link = $('<a>') // Crear un elemento <a> dinámico
-      .attr('href', './doc/public_alde.pem') // Ruta al archivo
-      .attr('download', 'Llave_Publica_Alde.pem') // Nombre del archivo descargado
-      .appendTo('body'); // Añadir al DOM
-    link[0].click(); // Simular clic en el enlace
-    link.remove(); // Eliminar el enlace después de la descarga
+      // Crea un enlace temporal para descargar el archivo
+    var link = document.createElement('a');
+    link.href = './doc/public_alde.pem'; // Ruta del archivo PDF
+    link.download = 'Llave_Publica_Alde.pem'; // Nombre del archivo descargado
+
+    // Añade el enlace temporal al DOM, dispara el evento click y lo elimina
+    $('body').append(link);
+    link.click();
+    $(link).remove();
   });
 });
