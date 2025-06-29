bbcc18ecc7419426525edf4506e448db2fd2559d
Alde_24 <aldeb2411@gmail.com>
2025-06-28 20:23:20 -0600

    Actualizacion index

--- javascript/index.js
+++ javascript/index.js
@@ -10,17 +10,5 @@ $(document).ready(function() {
     link.click();
     $(link).remove();
   });
-  // Descarga la llave pública
-  $('#btn_key').on('click', function() {
-      console.log("Descargando");
-      // Crea un enlace temporal para descargar el archivo
-    var link = document.createElement('a');
-    link.href = './doc/public_alde_2048.pem'; // Ruta del archivo PDF
-    link.download = 'Llave_Publica_Alde.pem'; // Nombre del archivo descargado
-
-    // Añade el enlace temporal al DOM, dispara el evento click y lo elimina
-    $('body').append(link);
-    link.click();
-    $(link).remove();
-  });
+  
 });
